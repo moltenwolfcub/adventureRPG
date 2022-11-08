f38e4e5835c1e6eec12caf3cb5b04072342a7359
MoltenWolfCub <oliver@tomlins.org.uk>
2022-11-08 20:11:27 +0000

    Initial commit
    libGDX setup

---
+++ desktop/src/com/moltenwolfcub/adventurerpg/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.moltenwolfcub.adventurerpg;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.moltenwolfcub.adventurerpg.Rpg;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Adventure RPG");
+		new Lwjgl3Application(new Rpg(), config);
+	}
+}
